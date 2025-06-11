edb32167619db9e706099099e2135cd3b5de0b33
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-06-11 14:03:00 +0530

    added health check test

---
+++ src/test/java/crud/TestHealthCheck.java
@@ -0,0 +1,31 @@
+package crud;
+
+import base.BaseTest;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import endpoints.APIConstants;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.TmsLink;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class TestHealthCheck extends BaseTest {
+
+    @Test(groups = "reg", priority = 2)
+    @TmsLink("https://bugz.atlassian.net/browse/TS-1")
+    @Owner("Abhishek")
+    @Description("TC#2  - Create Token and Verify")
+
+    public void testHealthCheckPost(){
+
+        //preparing the request
+        requestSpecification.basePath(APIConstants.PING_URL);
+
+        //response
+
+        response = RestAssured.given(requestSpecification).when().get();
+
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(201);
+    }
+}
